2ec1ac9a320f296c7bfbb331bd0eb062c1f7611e
jennifer <1793880774@qq.com>
2019-05-05 06:25:53 +0000

    debug

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    //aaa啊啊啊
     public function addUser(Request $request){
         $user_id = $request->input('user_id');
         echo $user_id;die;
